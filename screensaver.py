cf246e0bc3374f52124abf775977b5a16bcbaaf9
Dylan157 <dylan15766@hotmail.co.uk>
2016-11-18 21:20:04 +0000

    Add files via upload
    Edit config variables to change:
    map size - (may_xy)
    trail length

---
+++ screensaver.py
@@ -0,0 +1,132 @@
+from random import randint
+import time
+from sys import platform
+import os
+
+if platform == "linux" or platform == "linux2":
+    clear = lambda: os.system('clear')
+elif platform == "darwin":
+    clear = lambda: os.system('clear')
+elif platform == "win32":
+    clear = lambda: os.system('cls')
+
+#config Varibles 
+map_xy = 28
+trail_len = 280
+
+#boring varibles 
+screen = []
+blank_icon = " "
+player_icon = "#"
+player_xy = [1,0]
+move = []
+trail = []
+
+
+for click in range(map_xy):
+    screen.append([blank_icon] * map_xy)
+
+def print_board(board):
+    for row in board:
+        for x in range(1):
+            print " ".join(row)
+print_board(screen)
+
+def player_moves():
+    global move # move generator
+    move = []
+    move.append(randint(0, 2))
+    if move[0] != 1:
+        move.append(randint(0, 2))
+    else:
+        move.append(randint(0, 1))
+    return move
+
+def board_transport(move_choice):
+    global player_xy #Player xy_location changer based on player_move() output and move logic (if xy != off-map or trail)
+    global safety
+    safety += 1
+    if safety > 100:
+        player_xy = [randint(0, len(screen)-1), randint(0, len(screen)-1)]
+    chance = randint(0, map_xy)
+    if chance == map_xy: player_moves()
+    if move_choice[0] == 0:
+        if move_choice[1] == 0 and (player_xy[0] - 1 >= 0) and (player_xy[1] - 1 >= 0):
+            if screen[(player_xy[0] - 1)][(player_xy[1] - 1)] not in (player_icon):
+                player_xy[0] -= 1
+                player_xy[1] -= 1
+            else:
+                board_transport(player_moves())
+        elif move_choice[1] == 1 and (player_xy[0] - 1 >= 0):
+            if screen[(player_xy[0] - 1)][(player_xy[1])] not in (player_icon):
+                player_xy[0] -= 1
+            else:
+                board_transport(player_moves())
+        elif move_choice[1] == 2 and (player_xy[0] - 1 >= 0) and (player_xy[1] + 1 <= len(screen)-1):
+            if screen[(player_xy[0] - 1)][(player_xy[1] + 1)] not in (player_icon):
+                player_xy[0] -= 1
+                player_xy[1] += 1
+            else:
+                board_transport(player_moves())
+        else:
+            board_transport(player_moves())
+
+    elif move_choice[0] == 1:
+        if move_choice[1] == 0 and (player_xy[1] - 1 >= 0):
+            if screen[(player_xy[0])][(player_xy[1] - 1)] not in (player_icon):
+                player_xy[1] -= 1
+            else:
+                board_transport(player_moves())
+        elif move_choice[1] == 1 and (player_xy[1] + 1 <= len(screen)-1):
+            if screen[(player_xy[0])][(player_xy[1] + 1)] not in (player_icon):
+                player_xy[1] += 1
+            else:
+                board_transport(player_moves())
+        else:
+            board_transport(player_moves())
+
+    elif move_choice[0] == 2:
+        if move_choice[1] == 0 and (player_xy[0] + 1 <= len(screen)-1) and (player_xy[1] - 1 >= 0):
+            if screen[(player_xy[0] + 1)][(player_xy[1] - 1)] not in (player_icon):
+                player_xy[0] += 1
+                player_xy[1] -= 1
+            else:
+                board_transport(player_moves())
+        elif move_choice[1] == 1 and (player_xy[0] + 1 <= len(screen)-1):
+            if screen[(player_xy[0] + 1)][(player_xy[1])] not in (player_icon):
+                player_xy[0] += 1
+            else:
+                board_transport(player_moves())
+        elif move_choice[1] == 2 and (player_xy[0] + 1 <= len(screen)-1) and (player_xy[1] + 1 <= len(screen)-1):
+            if screen[(player_xy[0] + 1)][(player_xy[1] + 1)] not in (player_icon):
+                player_xy[0] += 1
+                player_xy[1] += 1
+            else:
+                board_transport(player_moves())
+        else:
+            board_transport(player_moves())
+
+def draw():
+    clear()
+    screen[player_xy[0]][player_xy[1]] = player_icon
+    print_board(screen)
+    trail.append(player_xy[:])
+    if len(trail) > trail_len:
+        screen[trail[0][0]][trail[0][1]] = blank_icon
+        del(trail[0])
+    time.sleep(0.066666666)
+
+
+        
+print player_moves()
+
+while True:
+    safety = 0
+    if move == None:
+        board_transport(player_moves())
+    else:
+        board_transport(move)
+    draw()
+
+    
+
